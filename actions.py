dd695830438eba787aaea0b7abc354f4188a0190
Ikey Doherty <ikey@solus-project.com>
2016-02-25 23:43:09 +0000

    Rebuild for 4.4.3
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- actions.py
+++ actions.py
@@ -9,7 +9,7 @@ import commands
 wdir = "NVIDIA-Linux-x86_64-%s-no-compat32" % get.srcVERSION()
 
 # Required... built in tandem with kernel update
-kversion = "4.4.2"
+kversion = "4.4.3"
 
 def setup():
     shelltools.system("sh NVIDIA-Linux-x86_64-%s-no-compat32.run --extract-only" % get.srcVERSION())
