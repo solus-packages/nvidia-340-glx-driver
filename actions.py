1eeb5980909d368e29150bc559efd18b31f95c45
Ikey Doherty <ikey@solus-project.com>
2016-12-16 19:02:29 +0000

    Update to 340.101
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- actions.py
+++ actions.py
@@ -15,7 +15,6 @@ def setup():
     shelltools.system("sh NVIDIA-Linux-x86_64-%s.run --extract-only" % get.srcVERSION())
     shelltools.cd(wdir)
     shelltools.system("patch -p0 < ../nv-drm.patch")
-    shelltools.system("patch -p1 < ../0001-os-mlock-Port-to-Linux-4.7-API.patch")
 
 def build():
     shelltools.cd(wdir + "/kernel")
