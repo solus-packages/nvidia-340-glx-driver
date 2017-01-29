905deea7e35a7de706f369429e78e1c9e6810777
Ikey Doherty <ikey@solus-project.com>
2017-01-29 03:06:40 +0000

    Rebuild for 4.9.6 kernel
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- actions.py
+++ actions.py
@@ -9,12 +9,13 @@ import commands
 wdir = "NVIDIA-Linux-x86_64-%s" % get.srcVERSION()
 
 # Required... built in tandem with kernel update
-kversion = "4.8.15"
+kversion = "4.9.6"
 
 def setup():
     shelltools.system("sh NVIDIA-Linux-x86_64-%s.run --extract-only" % get.srcVERSION())
     shelltools.cd(wdir)
     shelltools.system("patch -p0 < ../nv-drm.patch")
+    shelltools.system("patch -p0 < ../linux49.patch")
 
 def build():
     shelltools.cd(wdir + "/kernel")
